597aff22fe0d64f5b44a57111d25dc816bf76e38
Jose Lucas <j.lucas.romualdo@gmail.com>
2024-08-14 22:22:14 -0300

    primeiro commit

---
+++ AULA-02/ATIVIDADE/app.js
@@ -0,0 +1,5 @@
+alert('Olá, Fulano, tudo bem? Insira um valor inteiro abaixo');
+const valor1 = Number(prompt('Valor Inteiro'));
+const valor2 = Number(prompt('Insira outro valor inteiro'));
+
+console.log(d)
