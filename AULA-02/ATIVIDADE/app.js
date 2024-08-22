11e406c9d34cf43c069e978e1192e6e15294b7b1
José Lucas Romualdo Dias <j.lucas.romualdo@gmail.com>
2024-08-21 22:27:40 -0300

    Atualização do exercício e pasta da aula-04

--- AULA-02/ATIVIDADE/app.js
+++ AULA-02/ATIVIDADE/app.js
@@ -1,2 +1,12 @@
-const name = parseFloat(prompt('Insira seu nome'));
-console.log(name)
+const nomeUser = prompt('Insira o seu nome');
+    alert('Olá,', nomeUser);
+
+const valorDado = prompt ('Agora', nomeUser + '' +'insira um texto');
+    alert('O texto que você escreveu é', valorDado);
+const valorNum = Number(prompt('Insira um valor'));
+    console.log('O valor que você escreveu é', valorNum);
+
+const valorSoma = (valorDado + valorNum);
+    console.log('Pronto, agora vamos concatena-lo', valorSoma);
+
+    alert('Fim da operação');
---
+++ AULA-04/app.js
@@ -0,0 +1,32 @@
+const valor = Number(prompt('Insira um valor'));
+
+if (valor => 10 && valor <= 10){
+    alert('O valor está entre 10 e 50', valor);
+} else{
+    alert('O valor está fora entre 10 e 50', valor);
+}
+
+// novo desafio
+
+function pegarNumero(){
+    const valor = Number(prompt('Insira um valor'));
+    if (valor % 2 === 0){
+        alert('O valor digitado é par', valor);
+    } else {
+        alert('O valor digitado é ímpar', valor);
+    }
+}
+
+pegarNumero();
+pegarNumero();
+pegarNumero();
+
+//Calculadora
+
+function num (valor1,valor20){
+    if (valor1 => valor2){
+
+    } else (valor1 <= valor2) {
+        
+    }
+}
